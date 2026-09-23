377f3c9eec00b40aceaf75127307d891a68603b2
Olayeni <135321582+Olayeniii@users.noreply.github.com>
2026-09-23 09:49:13 +0100

    feat: restrict test mint to fixed Jara wallets

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,9 +6,14 @@ import {JaraInkTestNFT} from "../src/JaraInkTestNFT.sol";
 
 contract DeployJaraInkTestNFT is Script {
     function run() external returns (JaraInkTestNFT deployed) {
-        uint256 deployerKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        vm.startBroadcast(deployerKey);
-        deployed = new JaraInkTestNFT();
+        address[3] memory approvedWallets = [
+            vm.envAddress("JARA_EXECUTION_WALLET_1"),
+            vm.envAddress("JARA_EXECUTION_WALLET_2"),
+            vm.envAddress("JARA_EXECUTION_WALLET_3")
+        ];
+
+        vm.startBroadcast();
+        deployed = new JaraInkTestNFT(approvedWallets);
         vm.stopBroadcast();
     }
 }
